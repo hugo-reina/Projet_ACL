01fc3aae59d2a6b88158270878421cbcdc707f76
hugoreina <hugoreina.hr@gmail.com>
2023-10-05 20:59:44 +0200

    tout

--- src/main/java/com/example/jeu/HelloApplication.java
+++ src/main/java/com/example/jeu/HelloApplication.java
@@ -1,13 +1,17 @@
 package com.example.jeu;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -20,4 +24,8 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
+
 }
+
--- src/main/java/com/example/jeu/HelloController.java
+++ src/main/java/com/example/jeu/HelloController.java
@@ -1,14 +1,26 @@
 package com.example.jeu;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class HelloController {
+import java.io.IOException;
+import java.util.Objects;
+
+public class HelloController{
     @FXML
     private Label welcomeText;
-
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
     @FXML
-    protected void onHelloButtonClick() {
+    public void onHelloButtonClick() {
         //création du paquet de carte
         Paquet paquet = new Paquet();
         //melange
@@ -17,5 +29,18 @@ public class HelloController {
         Carte c1;
         c1 = paquet.piocherCarte();
         welcomeText.setText(c1.couleur + " " + c1.number);
+    }
+
+    public void QuitterPartie(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+    public void ScenePseudo(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/jeu/choix-pseudo.fxml"));
+        root = fxmlLoader.load();
+        stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
